f0c6661e4de41025a61d52e260adcd29520d15d2
rolandobrown <rolando.brown@gmail.com>
2015-08-12 14:21:15 -0400

    add parse_json method

--- gen_object.rb
+++ gen_object.rb
@@ -1,4 +1,5 @@
 # require_relative "./key.rb"
+require_relative 'cli_object.rb'
 require 'pry'
 require 'json'
 
@@ -6,32 +7,32 @@ class Genius
   attr_reader :song_source, :song_title, :song_lyrics
 
   def initialize
-    @song_lyrics = song_lyrics
-    @song_title = song_title
-    @song_source = song_source
+    # @song_lyrics = song_lyrics
+    # @song_title = song_title
+    # @song_source = song_source
+    @@song_title = ""
   end
 
-  def parse_gen_json(*words)
-
-    if love # replace with @@words
+  def parse_gen_json(@@words)
+    if @@words = "love"
         file_love = File.read('love.json')
         love_hash = JSON.parse(file_love)
-        today_songs_array ["response"]["hits"]["result"]["title"]
-
-    end
-    if learn # replace with @@words
+        today_love_songs_array ["response"]["hits"]["result"]["title"]
+        today_love_song = today_love_songs_array.collect {|doc| doc ["response"]["hits"]["result"]["title"]}
+        @@song_title << today_love_song
+    elsif  @@words = "learn"
       file_learn = File.read('learn.json')
       learn_hash = JSON.parse(file_learn)
-      today_songs_array ["response"]["hits"]["result"]["title"]
-
-    end
-    if code # replace with @@words
+      today_learn_songs_array ["response"]["hits"]["result"]["title"]
+      today_learn_song = today_learn_songs_array.collect {|doc| doc ["response"]["hits"]["result"]["title"]}
+      @@song_title << today_love_song
+    elsif @@words = "code" # replace with @@words
       file_code = File.read('code.json')
       code_hash = JSON.parse(file_code)
-      today_songs_array ["response"]["hits"]["result"]["title"]
-
+      today_code_songs_array ["response"]["hits"]["result"]["title"]
+      today_code_song = today_love_songs_array.collect {|doc| doc ["response"]["hits"]["result"]["title"]}
+      @@song_title << today_code_song
     end
-
   end
 
   def return_code_song_name(code_hash)

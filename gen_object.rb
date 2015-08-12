2767282d05833748f40a8769b013a4e5cb58ac46
rolandobrown <rolando.brown@gmail.com>
2015-08-12 12:28:44 -0400

    add draft of parse_gen_json method

--- gen_object.rb
+++ gen_object.rb
@@ -1,4 +1,6 @@
+# require_relative "./key.rb"
 require 'pry'
+require 'json'
 
 class Genius
   attr_reader :song_source, :song_title, :song_lyrics
@@ -9,13 +11,28 @@ class Genius
     @song_source = song_source
   end
 
-  file_love = File.read('love.json')
-  file_learn = File.read('learn.json')
-  file_code = File.read('code.json')
+  def parse_gen_json(*words)
 
-  learn_hash = JSON.parse(file_learn)
-  love_hash = JSON.parse(file_love)
-  code_hash = JSON.parse(file_code)
+    if love # replace with @@words
+        file_love = File.read('love.json')
+        love_hash = JSON.parse(file_love)
+        today_songs_array ["response"]["hits"]["result"]["title"]
+
+    end
+    if learn # replace with @@words
+      file_learn = File.read('learn.json')
+      learn_hash = JSON.parse(file_learn)
+      today_songs_array ["response"]["hits"]["result"]["title"]
+
+    end
+    if code # replace with @@words
+      file_code = File.read('code.json')
+      code_hash = JSON.parse(file_code)
+      today_songs_array ["response"]["hits"]["result"]["title"]
+
+    end
+
+  end
 
   def return_code_song_name(code_hash)
     code_hash.each do |k, v|

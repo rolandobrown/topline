06c1a9b17851aa708077b62fdf20e0bad26e4059
George Toothman <george.toothman@flatironschool.com>
2015-08-12 14:20:03 -0400

    json editing

--- gen_object.rb
+++ gen_object.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'json'
 
 class Genius
   attr_reader :song_source, :song_title, :song_lyrics
@@ -9,17 +10,18 @@ class Genius
     @song_source = song_source
   end
 
-  file_love = File.read('love.json')
-  file_learn = File.read('learn.json')
-  file_code = File.read('code.json')
+  # # file_love = File.read('love.json')
+  # file_learn = File.read('learn.json')
+  # # file_code = File.read('code.json')
 
-  learn_hash = JSON.parse(file_learn)
-  love_hash = JSON.parse(file_love)
-  code_hash = JSON.parse(file_code)
+  # learn_hash = JSON.parse(file_learn)
+  # love_hash = JSON.parse(file_love)
+  # code_hash = JSON.parse(file_code)
 
-  def return_code_song_name(code_hash)
-    code_hash.each do |k, v|
-      binding.pry
-    end
-  end  
+  # def return_code_song_name(learn_hash)
+  #   learn_hash.each do |k, v|
+  #     learn_hash["title"] 
+  #     binding.pry
+  #   end
+  # end  
 end

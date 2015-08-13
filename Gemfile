d9c85558d1be54a03280ae31bccd6bff26966df9
rolandobrown <rolando.brown@gmail.com>
2015-08-13 16:14:27 -0400

    add genius song url

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'pry'
 gem 'json', '~> 1.8.3'
+gem "genius", "~> 0.1.1"
--- cli_object.rb
+++ cli_object.rb
@@ -17,6 +17,7 @@ class Cli
     @time = Time.new.strftime("%Y%m%d")
     @@words = ""
     @@headlines_plus_songs = ""
+    @@songs = ""
   end
 
   def get_name
@@ -45,8 +46,28 @@ class Cli
     puts
   end
 
-  def map_array_with_index(array)
-    array1 = array.map.with_index(1){|element, index| puts "#{index}. " "#{element}" unless index > @@words.length}
+  def map_array_with_index_headline(array)
+    array1 = array.map.with_index(1){|element, index| puts "Headline #{index}. " "#{element}" unless index > @@words.length}
+    array1
+  end
+
+  def map_array_with_index_song(array)
+    array1 = array.map.with_index(1){|element, index| puts "Song #{index}. " "#{element}" unless index > @@words.length}
+    array1
+  end
+
+  def map_array_with_index_link(array)
+    array1 = array.map.with_index(1){|element, index| puts "Link #{index}. " "#{element}" unless index > @@words.length}
+    array1
+  end
+
+  def map_array_with_index_song_link(array)
+    array1 = array.map.with_index(1){|element, index| puts "Song url #{index}. " "#{element}" unless index > @@words.length}
+    array1
+  end
+
+  def map_array_with_index_artist(array)
+    array1 = array.map.with_index(1){|element, index| puts "Artist #{index}. " "#{element}" unless index > @@words.length}
     array1
   end
 
@@ -56,36 +77,38 @@ class Cli
     today_docs_array = today_parsed["response"]["docs"]
     today_headlines_array = today_docs_array.collect {|doc| doc["headline"]["main"]}
     today_headlines_link = today_docs_array.collect {|doc| doc["web_url"]}
-    map_array_with_index(today_headlines_array)
-    map_array_with_index(today_headlines_link)
+    map_array_with_index_headline(today_headlines_array)
+    map_array_with_index_link(today_headlines_link)
     puts
   end
 
   def parse_gen_json
     if @@words = "love"
-        file_love = File.read('love.json')
-        love_parsed = JSON.parse(file_love)
-        today_love_songs_array = love_parsed["response"]["hits"]
-        today_love_songs = today_love_songs_array.collect {|hit| hit ["result"]["title"]}
-        today_love_songs_link = today_love_songs_array.collect {|hit| hit["result"]["url"]}
-        map_array_with_index(today_love_songs)
-        map_array_with_index(today_love_songs_link)
+      file_love = File.read('love.json')
+      love_parsed = JSON.parse(file_love)
+      today_love_songs_array = love_parsed["response"]["hits"]
+      today_love_songs = today_love_songs_array.collect {|hit| hit ["result"]["title"]}
+      today_love_songs_artists = today_love_songs_array.collect {|hit| hit ["result"]["primary_artist"]["name"]}
+      today_love_songs_link = today_love_songs_array.collect {|hit| hit["result"]["url"]}
+      song = map_array_with_index_song(today_love_songs)
+      artist = map_array_with_index_artist(today_love_songs_artists)
+      link = map_array_with_index_song_link(today_love_songs_link)
     elsif  @@words = "learn"
       file_learn = File.read('learn.json')
       learn_parsed = JSON.parse(file_love)
       today_learn_songs_array = learn_parsed["response"]["hits"]
       today_learn_songs = today_learn_songs_array.collect {|hit| hit ["result"]["title"]}
       today_learn_songs_link = today_learn_songs_array.collect {|hit| hit["result"]["url"]}
-      map_array_with_index(today_learn_songs)
-      map_array_with_index(today_learn_songs_link)
+      map_array_with_index_song(today_learn_songs)
+      map_array_with_index_link(today_learn_songs_link)
     elsif @@words = "code"
       file_code = File.read('code.json')
       code_parsed = JSON.parse(file_love)
       today_code_songs_array = code_parsed["response"]["hits"]
       today_code_songs = today_code_songs_array.collect {|hit| hit ["result"]["title"]}
       today_code_songs_link = today_code_songs_array.collect {|hit| hit["result"]["url"]}
-      map_array_with_index(today_code_songs)
-      map_array_with_index(today_code_songs_link)
+      map_array_with_index_song(today_code_songs)
+      map_array_with_index_link(today_code_songs_link)
     end
     puts
   end
